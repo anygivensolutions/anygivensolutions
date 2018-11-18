2cff1d1c697f5c528d634630881a9126fc398256
anygivensolutions <anygivensolutions@users.noreply.github.com>
2018-11-18 11:30:47 -0800

    Add blog model

---
+++ models/blog.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    image: String,
+    tags: [],
+    entry: Number,
+    publish: String,
+    author: String,
+    category: String,
+    created: {type: Date, default: Date.now},
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
